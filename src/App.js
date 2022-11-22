8584b68359c438ce549900f5e160bcaf69c3ba73
Richardus Ferdian <50134930+ferdianfefe@users.noreply.github.com>
2022-11-22 17:59:32 +0700

    feat: add tailwind

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+
+const LoginCard = () => {
+  return (
+    <>
+      <div className="w-[50%] h-[400px] bg-white shadow-lg mx-auto mt-10">
+        <form></form>
+      </div>
+    </>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <LoginCard />
+      <div className="w-full"></div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
